323dafd878915440d74c9338aba5b805d35f98cd
Chad Smith <smith.chad12@gmail.com>
2022-01-13 19:49:50 -0600

    Initial Commit

---
+++ LocationMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocationMap
+//
+//  Created by Chad Smith on 1/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocationMap/LocationMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationMapApp.swift
+//  LocationMap
+//
+//  Created by Chad Smith on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct LocationMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
